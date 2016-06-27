0c40d7f9adfa1adb055f06d278b80b91dbc6b8db
majian <46617237@qq.com>
2016-06-27 10:37:48 +0800

    删除无用接口。

--- Rpc/Rabbit.Rpc/Transport/IMessageChannel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Rabbit.Rpc.Transport
-{
-    /// <summary>
-    /// 一个抽象的消息通道。
-    /// </summary>
-    public interface IMessageChannel : IMessageSender, IMessageListener
-    {
-    }
-}
