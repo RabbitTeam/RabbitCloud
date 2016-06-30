e9561f0b13d9e662c442be32e43b069bc6d77aa1
majian <46617237@qq.com>
2016-06-30 23:59:33 +0800

    优化客户端代理生成器。

--- Rpc/tools/Rabbit.Rpc.ClientGenerator/Program.cs
+++ Rpc/tools/Rabbit.Rpc.ClientGenerator/Program.cs
@@ -17,83 +17,129 @@ namespace Rabbit.Rpc.ClientGenerator
 {
     internal class Program
     {
-        private static void Main()
-        {
-            var assemblyFiles =
-                Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "assemblies"), "*.dll").ToArray();
-            var assemblies = assemblyFiles.Select(i => Assembly.Load(File.ReadAllBytes(i))).ToArray();
+        #region Field
 
-            IServiceProxyGenerater serviceProxyGenerater = new ServiceProxyGenerater(new DefaultServiceIdGenerator(new NullLogger<DefaultServiceIdGenerator>()));
+        /// <summary>
+        /// 输出路径。
+        /// </summary>
+        private static readonly string OutputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "outputs");
+
+        /// <summary>
+        /// 服务代理生成器。
+        /// </summary>
+        private static readonly IServiceProxyGenerater ServiceProxyGenerater = new ServiceProxyGenerater(new DefaultServiceIdGenerator(new NullLogger<DefaultServiceIdGenerator>()));
+
+        /// <summary>
+        /// 所获取到的程序集文件路径。
+        /// </summary>
+        private static readonly IEnumerable<string> AssemblyFiles;
+
+        /// <summary>
+        /// 加载到的程序集。
+        /// </summary>
+        private static readonly Assembly[] Assemblies;
+
+        /// <summary>
+        /// 命令方法字典。
+        /// </summary>
+        private static readonly Dictionary<string, Action> CommandActions = new Dictionary<string, Action>
+        {
+            {"1", GenerateAssembly},
+            {"2", GenerateCodeFiles}
+        };
+
+        #endregion Field
+
+        #region Constructor
+
+        static Program()
+        {
+            AssemblyFiles =
+                Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "assemblies"), "*.dll").ToArray();
+            Assemblies = AssemblyFiles.Select(i => Assembly.Load(File.ReadAllBytes(i))).ToArray();
 
             Console.WriteLine("成功加载了以下程序集");
-            foreach (var name in assemblies.Select(i => i.GetName().Name))
+            foreach (var name in Assemblies.Select(i => i.GetName().Name))
             {
                 Console.WriteLine(name);
             }
+        }
 
-            var services = assemblies
-                .SelectMany(assembly => assembly.GetExportedTypes())
-                .Where(i => i.IsInterface && i.GetCustomAttribute<RpcServiceAttribute>() != null);
+        #endregion Constructor
 
+        private static void Main()
+        {
             while (true)
             {
                 Console.WriteLine("1.生成客户端代理程序集");
                 Console.WriteLine("2.生成客户端代理代码");
 
-                var command = Console.ReadLine();
+                var command = Console.ReadLine() ?? string.Empty;
 
-                Func<IEnumerable<SyntaxTree>> getTrees = () =>
-                {
-                    var trees = new List<SyntaxTree>();
-                    foreach (var service in services)
-                    {
-                        trees.Add(serviceProxyGenerater.GenerateProxyTree(service));
-                    }
-                    return trees;
-                };
+                if (!Directory.Exists(OutputDirectory))
+                    Directory.CreateDirectory(OutputDirectory);
 
-                var outputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "outputs");
-                if (!Directory.Exists(outputDirectory))
+                Action action;
+                if (!CommandActions.TryGetValue(command, out action))
                 {
-                    Directory.CreateDirectory(outputDirectory);
+                    Console.WriteLine("无效的输入！");
+                    continue;
                 }
 
-                switch (command)
-                {
-                    case "1":
-                        var bytes = CompilationUtilitys.CompileClientProxy(getTrees(), assemblyFiles.Select(file => MetadataReference.CreateFromFile(file)));
-                        {
-                            var fileName = Path.Combine(outputDirectory, "Rabbit.Rpc.ClientProxys.dll");
-                            File.WriteAllBytes(fileName, bytes);
-                            Console.WriteLine($"生成成功，路径：{fileName}");
-                        }
-                        break;
-
-                    case "2":
-                        foreach (var syntaxTree in getTrees())
-                        {
-                            var className = string.Empty;
-                            var tempRoot = ((CompilationUnitSyntax)syntaxTree.GetRoot()).Members[0];
-                            if (tempRoot is NamespaceDeclarationSyntax)
-                            {
-                                className = ((tempRoot as NamespaceDeclarationSyntax).Members[0] as ClassDeclarationSyntax).Identifier.ValueText;
-                            }
-                            else if (tempRoot is ClassDeclarationSyntax)
-                            {
-                                className = (tempRoot as ClassDeclarationSyntax).Identifier.ValueText;
-                            }
-                            var code = syntaxTree.ToString();
-                            var fileName = Path.Combine(outputDirectory, $"{className}.cs");
-                            File.WriteAllText(fileName, code, Encoding.UTF8);
-                            Console.WriteLine($"生成成功，路径：{fileName}");
-                        }
-                        break;
-
-                    default:
-                        Console.WriteLine("无效的输入！");
-                        continue;
-                }
+                action();
             }
         }
+
+        #region Private Method
+
+        private static void GenerateAssembly()
+        {
+            var bytes = CompilationUtilitys.CompileClientProxy(GetTrees(), AssemblyFiles.Select(file => MetadataReference.CreateFromFile(file)));
+            {
+                var fileName = Path.Combine(OutputDirectory, "Rabbit.Rpc.ClientProxys.dll");
+                File.WriteAllBytes(fileName, bytes);
+                Console.WriteLine($"生成成功，路径：{fileName}");
+            }
+        }
+
+        private static void GenerateCodeFiles()
+        {
+            foreach (var syntaxTree in GetTrees())
+            {
+                var compilationUnitSyntax = (CompilationUnitSyntax)syntaxTree.GetRoot();
+
+                var classDeclarationSyntax = FindClassDeclaration(compilationUnitSyntax.Members);
+                var className = classDeclarationSyntax.Identifier.ValueText;
+                var code = syntaxTree.ToString();
+                var fileName = Path.Combine(OutputDirectory, $"{className}.cs");
+                File.WriteAllText(fileName, code, Encoding.UTF8);
+                Console.WriteLine($"生成成功，路径：{fileName}");
+            }
+        }
+
+        private static IEnumerable<SyntaxTree> GetTrees()
+        {
+            var services = Assemblies
+                .SelectMany(assembly => assembly.GetExportedTypes())
+                .Where(i => i.IsInterface && i.GetCustomAttribute<RpcServiceAttribute>() != null);
+            return services.Select(service => ServiceProxyGenerater.GenerateProxyTree(service));
+        }
+
+        private static ClassDeclarationSyntax FindClassDeclaration(IEnumerable<MemberDeclarationSyntax> members)
+        {
+            foreach (var member in members)
+            {
+                var classDeclaration = member as ClassDeclarationSyntax;
+                if (classDeclaration != null)
+                    return classDeclaration;
+
+                var namespaceDeclaration = member as NamespaceDeclarationSyntax;
+                if (namespaceDeclaration != null)
+                    return FindClassDeclaration(namespaceDeclaration.Members);
+            }
+            return null;
+        }
+
+        #endregion Private Method
     }
 }
