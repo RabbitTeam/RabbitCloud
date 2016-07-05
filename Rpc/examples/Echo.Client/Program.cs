b2262952baf23ac2ce160cf723f0fa98fbd24d6e
majian <46617237@qq.com>
2016-07-05 15:28:14 +0800

    美化代码。

--- Rpc/examples/Echo.Client/Program.cs
+++ Rpc/examples/Echo.Client/Program.cs
@@ -22,7 +22,7 @@ namespace Echo.Client
                 .AddLogging()
                 .AddClient()
                                 //                .UseJsonCodec()
-                                .UseProtoBufferCodec()
+                .UseProtoBufferCodec()
                 .UseSharedFileRouteManager("d:\\routes.txt")
                 //zookeeper服务路由管理者。
                 //                .UseZooKeeperRouteManager(new ZooKeeperServiceRouteManager.ZookeeperConfigInfo("172.18.20.132:2181"))
