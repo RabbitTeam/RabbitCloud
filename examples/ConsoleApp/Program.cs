2b19c3e2ca34ab73f9ea19ac81ad2821a554316c
majian <majian159@live.com>
2017-09-01 09:54:10 +0800

    使用c#7.1的异步main函数

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -7,34 +7,31 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             var consulClient = new ConsulClient(s =>
             {
                 s.Address = new Uri("http://192.168.1.150:8500");
             });
 
-            Task.Run(async () =>
+            var response = await consulClient.Agent.Services();
+            foreach (var item in response.Response.Values)
             {
-                var response = await consulClient.Agent.Services();
-                foreach (var item in response.Response.Values)
-                {
-                    await consulClient.Agent.ServiceDeregister(item.ID);
-                }
+                await consulClient.Agent.ServiceDeregister(item.ID);
+            }
 
-                var handler = new DiscoveryHttpClientHandler(new ConsulDiscoveryClient(consulClient), NullLogger<DiscoveryHttpClientHandler>.Instance);
-                var consulRegistryService = new ConsulRegistryService(consulClient);
-                await consulRegistryService.RegisterAsync(ConsulRegistration.Create("userService", new Uri("http://localhost:20578")));
+            var handler = new DiscoveryHttpClientHandler(new ConsulDiscoveryClient(consulClient), NullLogger<DiscoveryHttpClientHandler>.Instance);
+            var consulRegistryService = new ConsulRegistryService(consulClient);
+            await consulRegistryService.RegisterAsync(ConsulRegistration.Create("userService", new Uri("http://localhost:20578")));
 
-                var httpClient = new HttpClient(handler);
+            var httpClient = new HttpClient(handler);
 
-                var content = await httpClient.GetStringAsync("http://userService/User/GetUser/1");
-                Console.WriteLine(content);
-            }).Wait();
+            var content = await httpClient.GetStringAsync("http://userService/User/GetUser/1");
+            Console.WriteLine(content);
 
             Console.ReadLine();
         }
