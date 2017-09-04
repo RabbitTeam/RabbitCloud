bfbc1353c7eb62cfd9f5ad4286eb3f6dd97544ce
majian <majian159@live.com>
2017-09-04 10:35:37 +0800

    1.move extensions to new repository. 2.add build.

--- src/Rabbit.Extensions.Configuration/TemplateSupportOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rabbit.Extensions.Configuration
-{
-    public class TemplateSupportOptions : TemplateRenderOptions
-    {
-        public bool EnableChildren { get; set; } = true;
-        public bool ReloadOnChange { get; set; } = true;
-    }
-}
