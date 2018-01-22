f6eba45cee4d135d59e5c81984508ff69cbb561c
majian <majian159@live.com>
2018-01-22 21:52:18 +0800

    添加dingtalk的goclient

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -1,140 +1,72 @@
-﻿using Castle.DynamicProxy;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Rabbit.Cloud.Client;
-using Rabbit.Cloud.Client.Go;
-using Rabbit.Cloud.Client.Go.Abstractions;
-using Rabbit.Cloud.Client.Go.ApplicationModels;
-using Rabbit.Cloud.Client.Http;
-using Rabbit.Cloud.Discovery.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rabbit.DingTalk.Go;
+using Rabbit.Go.Abstractions;
+using Rabbit.Go.Core;
 using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    public class BookModel
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-    }
-
-    [GoClient("http://localhost:5000/cartoons")]
-    public interface ITestClient
-    {
-        [GoGet("/book/{query}")]
-        Task<BookModel[]> SayHelloAsync([GoParameter(ParameterTarget.Path)]string query,long id=1);
-    }
-
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.Run(async context =>
-            {
-                var result = new[]
-                {
-                    new BookModel
-                    {
-                        Id = 1,
-                        Name = "book"
-                    }
-                };
-                await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
-            });
-        }
-    }
-
     public class Program
     {
-        private static Task RunHttpServer()
-        {
-            return WebHost.CreateDefaultBuilder()
-                .ConfigureLogging(lb => { lb.ClearProviders(); })
-                .UseStartup<Startup>()
-                .UseUrls("http://localhost:5000")
-                .Build()
-                .RunAsync();
-        }
-
-        private class EmptyDiscoveryClient : IDiscoveryClient
-        {
-            #region Implementation of IDisposable
-
-            /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
-            public void Dispose()
-            {
-            }
-
-            #endregion Implementation of IDisposable
-
-            #region Implementation of IDiscoveryClient
-
-            public string Description { get; }
-
-            /// <summary>
-            /// all serviceId
-            /// </summary>
-            public IReadOnlyList<string> Services { get; }
-
-            public IReadOnlyList<IServiceInstance> GetInstances(string serviceId)
-            {
-                return new IServiceInstance[0];
-            }
-
-            #endregion Implementation of IDiscoveryClient
-        }
-
         public static async Task Main(string[] args)
         {
-            RunHttpServer();
+/*            var appServices=new ServiceCollection()
+                .AddOptions()
+                .AddLogging()
+                .BuildServiceProvider();
 
-            await Task.Delay(1000);
-            try
-            {
-                var applicationServices = new ServiceCollection()
-                    .AddLogging()
-                    .AddOptions()
-                    .AddSingleton<IDiscoveryClient, EmptyDiscoveryClient>()
-                    .BuildServiceProvider();
+            var app=new RabbitApplicationBuilder(appServices)
+                .UseRabbitClient()
+                .UseRabbitHttpClient()
+                .Build();*/
 
-                var services = new ServiceCollection()
-                    .AddRabbitClient(applicationServices, app =>
+            var dingTalkCodec = new DingTalkCodec();
+            var services=new ServiceCollection()
+                .AddOptions()
+                .Configure<GoOptions>(o =>
                     {
-                        app
-                            .UseRabbitClient()
-                            .UseRabbitHttpClient();
+                        o.FormatterMappings.Set("application/json", dingTalkCodec, dingTalkCodec);
                     })
-                    .AddSingleton(RabbitApplicationBuilder.BuildModel(new[] { typeof(ITestClient).GetTypeInfo() }))
-                    .AddSingleton<IInterceptor, DefaultInterceptor>()
-                    .AddGoClientProxy()
-                    .BuildServiceProvider();
+                .AddGo()
+                .BuildServiceProvider();
 
-                var testClient = services.GetRequiredService<ITestClient>();
-                var response = await testClient.SayHelloAsync("$filter=Id eq 220");
-                Console.WriteLine(JsonConvert.SerializeObject(response));
+            var goFactory = services.GetService<IGoFactory>();
 
-                var watch=Stopwatch.StartNew();
-                for (int i = 0; i < 10000; i++)
+            var dingTalkGoClient = goFactory.CreateInstance<IDingTalkGoClient>();
+
+            //            await dingTalkGoClient.SendAsync(new TextMessage("test"));
+            //            await dingTalkGoClient.SendAsync(new LinkMessage("title","text","http://www.baidu.com", "https://www.baidu.com/img/bd_logo1.png"));
+            //            return;
+            //            await dingTalkGoClient.SendAsync(new MarkdownMessage("杭州天气", "#### 杭州天气 @156xxxx8827") {IsAtAll = true});
+            //            return;
+            await dingTalkGoClient.SendAsync(
+                new SingleActionCardMessage("title", "text", "sTitle", "https://open-doc.dingtalk.com/")
                 {
-                    await testClient.SayHelloAsync("$filter=Id eq 220");
-                }
-                watch.Stop();
-                Console.WriteLine(watch.ElapsedMilliseconds+"ms");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+                    BtnOrientation = Orientation.Vertical,
+                    HideAvatar = false
+                });
+            return;
+            /*
 
-            Console.ReadLine();
+                        var message = new ActionCardMessage("乔布斯 20 年前想打造一间苹果咖啡厅，而它正是 Apple Store 的前身", @"![screenshot](@lADOpwk3K80C0M0FoA) 
+            ### 乔布斯 20 年前想打造的苹果咖啡厅 
+                        Apple Store 的设计正从原来满满的科技感走向生活化，而其生活化的走向其实可以追溯到 20 年前苹果一个建立咖啡馆的计划")
+                        {
+                            HideAvatar = true,
+                            BtnOrientation = Orientation.Horizontal
+                        };
+
+                        message.Buttons.Add(new ActionCardMessage.ActionCardButton("内容不错", "https://www.dingtalk.com/"));
+                        message.Buttons.Add(new ActionCardMessage.ActionCardButton("不感兴趣", "https://www.dingtalk.com/"));
+            */
+
+            var message = new FeedCardMessage();
+            message.Items.Add(new FeedCardMessage.FeedCardItem("title1","http://www.baidu.com", "https://www.baidu.com/img/bd_logo1.png"));
+            message.Items.Add(new FeedCardMessage.FeedCardItem("title2", "http://www.baidu.com", "https://www.baidu.com/img/bd_logo1.png"));
+            message.Items.Add(new FeedCardMessage.FeedCardItem("title3", "http://www.baidu.com", "https://www.baidu.com/img/bd_logo1.png"));
+
+            await dingTalkGoClient.SendAsync(message);
+
         }
     }
 }
---
+++ src/Rabbit.DingTalk.Go/IDingTalkGoClient.cs
@@ -0,0 +1,28 @@
+﻿using Rabbit.Go.Abstractions;
+using System.Threading.Tasks;
+
+namespace Rabbit.DingTalk.Go
+{
+    [Go("https://oapi.dingtalk.com/robot/send?access_token=token")]
+    [DefaultHeader("Content-Type", "application/json")]
+    public interface IDingTalkGoClient
+    {
+        [GoPost]
+        Task SendAsync([GoBody]TextMessage message);
+
+        [GoPost]
+        Task SendAsync([GoBody]LinkMessage message);
+
+        [GoPost]
+        Task SendAsync([GoBody]MarkdownMessage message);
+
+        [GoPost]
+        Task SendAsync([GoBody]SingleActionCardMessage message);
+
+        [GoPost]
+        Task SendAsync([GoBody]ActionCardMessage message);
+
+        [GoPost]
+        Task SendAsync([GoBody]FeedCardMessage message);
+    }
+}
